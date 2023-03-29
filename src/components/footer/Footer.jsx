98ae0d5a670dcf0c52e52f607771bd3c806c326d
ksky793 <k.sky793@gmail.com>
2023-03-29 02:33:16 +0200

    Dodano Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,15 @@
 import './Footer.scss';
 const Footer = () => {
-	return <footer>Footer</footer>;
+	return (
+		<footer className='footer' id='footer'>
+			<div className='footer__copyright m-t--10'>
+				<span className='footer__copyright__icon'>©</span>
+				<span className='footer__copyright__text'>
+					2022 Kamil Świętochowski
+				</span>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
