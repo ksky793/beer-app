138519a24430afafe9f6c0cdcf7bd73223b02070
ksky793 <k.sky793@gmail.com>
2023-03-30 08:00:32 +0200

    Dodano Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,68 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+// icons
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+// styles
 import './Navbar.scss';
 const Navbar = () => {
-	return <nav className='navbar'>Navbar</nav>;
+	const navList = [
+		{
+			name: 'Home',
+			link: '/',
+		},
+		{
+			name: 'Beers',
+			link: '/beers',
+		},
+		{
+			name: 'Contact',
+			link: '/contact',
+		},
+		{
+			name: 'About',
+			link: '/about',
+		},
+	];
+
+	const [showNavbar, setShowNavbar] = useState(false);
+
+	const toggleNavbar = () => {
+		setShowNavbar(!showNavbar);
+	};
+
+	const handleClickItem = () => {
+		if (showNavbar) {
+			setShowNavbar(false);
+		}
+	};
+	return (
+		<nav className='navbar'>
+			<div className='wrapper'>
+				<div className='navbar__logo'>
+					<Link to='/' className='navbar__logo__text'>
+						Beer<span>Mania+</span>
+					</Link>
+				</div>
+				<ul className={`navbar__links ${showNavbar && 'navbar__links--open'}`}>
+					{navList.map((el, index) => (
+						<li className='navbar__links__link' key={index}>
+							<Link to={el.link} onClick={handleClickItem}>
+								{el.name}
+							</Link>
+						</li>
+					))}
+				</ul>
+				<div className='navbar__icon' onClick={toggleNavbar}>
+					{showNavbar ? (
+						<AiOutlineClose className='ic' />
+					) : (
+						<FaBars className='ic navbar__menu' />
+					)}
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
